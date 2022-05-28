fa3d25423b53bab526b963f13e4dcd3ddaf04693
xiaoxian521 <1923740402@qq.com>
2022-05-28 17:47:21 +0800

    feat: add `darken`、`lighten` utils

--- src/index.js
+++ src/index.js
@@ -23,6 +23,37 @@ import { resetStylePreprocessor } from "./common/resetStylePreprocessor"
 // eslint-disable-next-line import/no-unresolved
 let preCustomThemeOutputPath = ""
 
+/** hex转rgb */
+function hexToRgb(str) {
+  let hxs = str.replace("#", "").match(/../g)
+  for (let i = 0; i < 3; i++) hxs[i] = parseInt(hxs[i], 16)
+  return hxs
+}
+
+/** rgb转hex */
+function rgbToHex(a, b, c) {
+  let hexs = [a.toString(16), b.toString(16), c.toString(16)]
+  for (let i = 0; i < 3; i++) {
+    if (hexs[i].length == 1) hexs[i] = `0${hexs[i]}`
+  }
+  return `#${hexs.join("")}`
+}
+
+/** 加深颜色值 */
+function darken(color, level) {
+  let rgbc = hexToRgb(color)
+  for (let i = 0; i < 3; i++) rgbc[i] = Math.floor(rgbc[i] * (1 - level))
+  return rgbToHex(rgbc[0], rgbc[1], rgbc[2])
+}
+
+/** 变浅颜色值 */
+function lighten(color, level) {
+  let rgbc = hexToRgb(color)
+  for (let i = 0; i < 3; i++)
+    rgbc[i] = Math.floor((255 - rgbc[i]) * level + rgbc[i])
+  return rgbToHex(rgbc[0], rgbc[1], rgbc[2])
+}
+
 /**
  * lang : "less" | "scss" | "sass"
  * @param {object} options : { [lang]:{ multipleScopeVars: [{scopeName:"theme-1",path: path.resolve('./vars.less')}], outputDir, defaultScopeName ,extract,removeCssScopeName,customThemeCssFileName,themeLinkTagId,themeLinkTagInjectTo } }
@@ -596,4 +627,6 @@ export {
   themePreprocessorPlugin,
   themePreprocessorHmrPlugin,
   resetStylePreprocessor,
+  darken,
+  lighten
 }
